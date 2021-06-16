a588f6012e0ddf997a6e93b278bb8c67ebc137e4
RoyChuang <s041516@apps.ntpc.edu.tw>
2021-06-16 14:00:21 +0800

    Inital Commit

---
+++ blink/blink.ino
@@ -0,0 +1,10 @@
+void setup(){  // put your setup code here, to run once:
+  pinMode( 13 , OUTPUT);   // sets the digital pin as output
+}
+
+void loop(){  // put your main code here, to run repeatedly:
+  digitalWrite( 13 , HIGH );  // sets the digital pin on/off
+  delay( 1000 ); // waits a few milliseconds 
+  digitalWrite( 13 , LOW );  // sets the digital pin on/off
+  delay( 1000 ); // waits a few milliseconds 
+}
